079129ebc14ace50c946e911edefdfd2554a5150
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2025-04-27 01:00:30 +0300

    fix the Navbar Error

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Menu, User, LogOut } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
 import {
   DropdownMenu,
   DropdownMenuContent,
