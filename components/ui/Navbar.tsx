105848972265dd5bf5ae2b20e2c8faac50865c5a
ABDULLAHI ABDIRIZAK MOHAMED <105462733+Abdallahdari@users.noreply.github.com>
2025-04-27 14:54:19 +0300

    change the toggle in navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -15,11 +15,14 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Signout } from "@/app/_lib/action";
 
+interface NavbarProps {
+  toggleSidebar: () => void;
+}
 const Logout = async () => {
   await Signout();
 };
 
-export function Navbar({ toggleSidebar, session }) {
+export function Navbar({ toggleSidebar, session }: NavbarProps) {
   return (
     <header className="flex items-center justify-between py-4 border-b border-gray-200 my-4">
       <h1 className="text-3xl font-bold">Dalab AdminPanel</h1>
